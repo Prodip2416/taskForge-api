d8570ed9b8d28f3fbdc7e1739150325ade2190b2
Prodip <prodip.sarker.cse@gmail.com>
2026-03-03 11:48:22 +0600

    added db connection by using config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from 'src/config/database.config';
 
+const ENV = process.env.NODE_ENV ?? 'development';
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+     ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.dbName'),
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, length: 200 })
+  firstName: string;
+
+  @Column({ nullable: true, length: 200 })
+  lastName: string;
+
+  @Column({ unique: true, nullable: false, length: 200 })
+  email: string;
+
+  @Column({ nullable: false, length: 500 })
+  password: string;
+
+  @Column({ default: true, nullable: false })
+  isActive: boolean;
+
+  @Column({ default: false, nullable: false })
+  isDelete: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
